4b23cc04a567ac074410801bbe497feb823c6e16
marianekk578@gmail.com <Xdmarian.578>
2019-09-23 20:59:15 +0200

    mistake fixed

--- src/sorting/Quicksort.java
+++ src/sorting/Quicksort.java
@@ -7,25 +7,20 @@ public class Quicksort {
         int[] array = {7, 3, 2, 8, 5, 9, 4, 1};
 
 
-        quicksort(array,0,array[array.length-1]);
+        quicksort(array,0,array.length-1);
 
     }
 
     public static void quicksort(int array[], int low, int high){
 
-        int pivot = array[array.length-1];
+        int pivot = array[array.length/2];
 
         if(array.length==0 || array.length==1)
             return;
 
-        for (int i: array) {
-            if(i<pivot)
-                i++;
 
 
-        }
 
-        System.out.println(pivot);
 
     }
 }
