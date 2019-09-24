dab478a097dbd08529294197a26f78b8d89a5460
marianekk578@gmail.com <Xdmarian.578>
2019-09-24 14:48:17 +0200

    whole algorithm implemented

--- src/sorting/Quicksort.java
+++ src/sorting/Quicksort.java
@@ -1,26 +1,54 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class Quicksort {
 
     public static void main(String[] args) {
 
         int[] array = {7, 3, 2, 8, 5, 9, 4, 1};
 
-
-        quicksort(array,0,array.length-1);
+        int n = array.length;
+        quicksort(array, 0, n - 1);
+        System.out.println(Arrays.toString(array));
 
     }
 
-    public static void quicksort(int array[], int low, int high){
+    public static void quicksort(int array[], int low, int high) {
 
-        int pivot = array[array.length/2];
+        if (low >= high) {
+            return;
+        }
 
-        if(array.length==0 || array.length==1)
+        if (array.length <= 1 || array == null)
             return;
 
+        int pivot = array[high];
+        int border = low - 1;
+        int i = low;
 
+        while (i < high) {
+            if (array[i] < pivot) {
+                border++;
+                if (border != i) {
+                    swap(array, border, i);
+                }
+            }
+            i++;
+        }
 
+        border++;
+        if (border != high) {
+            swap(array, border, high);
+        }
 
+        quicksort(array, low, border - 1);
+        quicksort(array, border + 1, high);
+    }
 
+    public static void swap(int[] array, int firstElement, int secondElement) {
+        int temp = array[firstElement];
+        array[firstElement] = array[secondElement];
+        array[secondElement] = temp;
     }
 }
