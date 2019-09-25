e7b9d85b06eb63c2783ae4d315b5b42ececdc91b
marianekk578@gmail.com <Xdmarian.578>
2019-09-25 12:30:28 +0200

    documentation added

--- src/sorting/Quicksort.java
+++ src/sorting/Quicksort.java
@@ -2,6 +2,10 @@ package sorting;
 
 import java.util.Arrays;
 
+/**
+ * Quicksort Algorithm
+ * by Marcin Wójcik
+ */
 public class Quicksort {
 
     public static void main(String[] args) {
@@ -15,7 +19,7 @@ public class Quicksort {
     }
 
     public static void quicksort(int array[], int low, int high) {
-
+        
         if (low >= high) {
             return;
         }
@@ -23,10 +27,12 @@ public class Quicksort {
         if (array.length <= 1 || array == null)
             return;
 
+        /** get the pivot element from the end */
         int pivot = array[high];
         int border = low - 1;
         int i = low;
 
+        /** divide array into two groups, first one for elements smaller than pivot, second for higher */
         while (i < high) {
             if (array[i] < pivot) {
                 border++;
@@ -37,11 +43,13 @@ public class Quicksort {
             i++;
         }
 
+        /** getting pivot between two group */
         border++;
         if (border != high) {
             swap(array, border, high);
         }
 
+        /** recursive operations for tow sub arrays */
         quicksort(array, low, border - 1);
         quicksort(array, border + 1, high);
     }
